65e34dd0e2d0d93c62a3f8a8e55e8d983c085607
dijalaanita <100611369+dijalaanita@users.noreply.github.com>
2023-11-25 15:58:23 +0000

    write code

---
+++ COURSEWORK 3/MEMBERSHIP FEES INCREASE/membershipFeesInc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    float membership = 2500.0, feeRate = 0.04, feeNum, newFees;
+    int year = 6;
+
+    for (int period = 1; period <= year; period++)
+    {
+        feeNum = membership * feeRate;
+        newFees = feeNum + membership;
+        printf("%d %.2f",period, newFees);
+    }
+    
+    
+}

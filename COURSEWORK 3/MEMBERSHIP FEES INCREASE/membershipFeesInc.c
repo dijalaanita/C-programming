8747eae3296830f4e1d5ecf6ab7c5d1c790ce377
dijalaanita <100611369+dijalaanita@users.noreply.github.com>
2023-11-25 16:13:14 +0000

    update comment for user

--- COURSEWORK 3/MEMBERSHIP FEES INCREASE/membershipFeesInc.c
+++ COURSEWORK 3/MEMBERSHIP FEES INCREASE/membershipFeesInc.c
@@ -3,14 +3,15 @@
 
 int main(void)
 {
-    float membership = 2500.0, feeRate = 0.04, feeNum, newFees;
+    float membership = 2500.0, feeRate = 0.04, feeNum, newFees; //declare and initialize variables
     int year = 6;
 
-    for (int period = 1; period <= year; period++)
+    for (int period = 1; period <= year; period++)//iteration to update memebership price each year
     {
+        //calculating the new fees for each year
         feeNum = membership * feeRate;
         newFees = feeNum + membership;
-        printf("%d $%.1f\n",period, newFees);
+        printf("%d $%.1f\n",period, newFees);//displaying the year and the price increase
 
         // Update membership for the next iteration
         membership = newFees;
